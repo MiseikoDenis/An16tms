abf391528771216491823f2ecd0ad15c802ed106
MiseikoDenis <68539464+MiseikoDenis@users.noreply.github.com>
2022-07-11 16:47:13 +0300

    Delete main directory

--- main/java/com/example/an16tms/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.an16tms
-
-data class User (val name: String, val secondName: String, val age: Int){
-
-    override fun toString(): String {
-        return "Пользователь с именем $name, фамилией $secondName в возрасте $age"
-    }
-}
